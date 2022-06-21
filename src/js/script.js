62889015b3a2732628e3c675a16185852b70ab7d
FroL432 <frolikf2020@gmail.com>
2022-06-21 21:47:21 +0300

    adaptive 4 section+carousel

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
 // alert ();
 $(document).ready(function(){
     $('.carousel__inner').slick({
+        infinite: true,
         speed: 1200,
         // adaptiveHeight: true,
         autoplay: true,
