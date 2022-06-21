f27e3c80ff0291acc72ef012720f4d6600335618
FroL432 <frolikf2020@gmail.com>
2022-06-21 21:32:36 +0300

    adaptive 4 section+carousel

--- src/js/script.js
+++ src/js/script.js
@@ -3,20 +3,32 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
         // adaptiveHeight: true,
-        autoplay: true,
-        autoplaySpeed: 2000,
+        // autoplay: true,
+        // autoplaySpeed: 2000,
         // fade: true,
         // cssEase: 'linear'
         prevArrow: '<button type="button" class="slick-prev"><img src="./img/red_left_btn.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="./img/red_right_btn.png"></button>',
+        // variableWidth: true,
+        // adaptiveHeight: true,
         responsive: [
             {
                 breakpoint: 992,
                 settings: {
-                    dots: true,
+                    dots: false,
                     arrows: false
                 }
             }
+            // {
+            //     breakpoint: 768,
+            //     settings: {
+            //     }
+            // }
+            // {
+            //     breakpoint: 575,
+            //     settings: {
+            //     }
+            // }
         ]
     });
 });
